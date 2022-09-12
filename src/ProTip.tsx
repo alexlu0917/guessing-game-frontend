c25f4e02c31283da303bf5b3a205c7c2b76ee75c
alexlu0917 <alexlu0917.dev@gmail.com>
2022-09-12 13:34:30 +0100

    update tooltip

--- src/ProTip.tsx
+++ src/ProTip.tsx
@@ -16,10 +16,10 @@ export default function ProTip() {
     <Typography sx={{ mt: 6, mb: 3 }} color="text.secondary">
       <LightBulbIcon sx={{ mr: 1, verticalAlign: "middle" }} />
       Pro tip: See more{" "}
-      <Link href="https://mui.com/getting-started/templates/">
-        templates
+      <Link href="https://github.com/alexlu0917">
+        recent work
       </Link>{" "}
-      on the MUI documentation.
+      on my Github.
     </Typography>
   );
 }
